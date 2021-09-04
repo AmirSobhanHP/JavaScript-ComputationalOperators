4788412c8fcb932db005bcf5658eff6fa975811e
AmirsobhanHP <persiandownload2021@gmail.com>
2021-09-04 19:50:26 +0430

    first commit

---
+++ Computational Operators.js
@@ -0,0 +1,18 @@
+let number_1 = 10;
+let number_2 = 5;
+console.log(number_1 + number_2); // Addition(sum) => +
+console.log(number_1 - number_2); // Subtract => -
+console.log(number_1 * number_2); // Multiply => *
+console.log(number_1 / number_2); // Division => /
+console.log(number_1 ** number_2); // Power => **
+console.log(number_1 ** (1/2)); // square root => ** (1/2)
+console.log(number_1 ** (1/3)); // cube root => ** (1/3)\
+console.log(number_1 % number_2); // mudulus => % 
+// console.log(++number_1); // number_1 = number_1 + 1 
+// console.log(--number_1); // number_1 = number_1 - 1
+// console.log(number_1 += number_2); // number_1 = number_1 + number_2
+// console.log(number_1 -= number_2); // number_1 = number_1 - number_2
+// console.log(number_1 *= number_2); // number_1 = number_1 * number_2
+// console.log(number_1 /= number_2); // number_1 = number_1 / number_2 
+// console.log(number_1 %= number_2); // number_1 = number_1 % number_2
+// console.log(number_1 **= number_2); // number_1 = number_1 ** number_2
